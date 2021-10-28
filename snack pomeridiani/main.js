1cb51caee0a0ef4f1b5557451ca2313ebf5ce57e
ieeah <lbnfnc90@gmail.com>
2021-10-28 17:45:34 +0200

    add for loop

--- snack pomeridiani/main.js
+++ snack pomeridiani/main.js
@@ -4,21 +4,28 @@ const btn = document.querySelector('button');
 
 
 let number = '';
+let somma = 0;
+let media = 0;
+
 btn.addEventListener('click', function () {
     number = prompt('inserisci un numero di quattro cifre');
 
-    while ((number.length > 4) || (number.length < 4)) {
+    while ((number.length > 4) || (number.length < 4) || (isNaN(number))) {
         number = prompt('inserisci un numero di quattro cifre');
     }
 
-    let realNumber = parseInt(number);
-    let n1 = parseInt(number[0]);
-    let n2 = parseInt(number[1]);
-    let n3 = parseInt(number[2]);
-    let n4 = parseInt(number[3]);
+    for (let i = 0; i < number.length; i++ ) {
+        somma += parseInt(number[i]);
+        console.log(parseInt(number[i]));
+    }
 
-    let somma = n1 + n2 + n3 + n4;
-    let media = somma / 4;
+    media = somma / number.length;
+
+    // let realNumber = parseInt(number);
+    // let n1 = parseInt(number[0]);
+    // let n2 = parseInt(number[1]);
+    // let n3 = parseInt(number[2]);
+    // let n4 = parseInt(number[3]);
 
     document.body.innerHTML = `<h3>la somma delle singole cifre del tuo numero è${somma}.</h3>
     <h3>la media della somma delle cifre del tuo numero è${media}.</h3>`;
